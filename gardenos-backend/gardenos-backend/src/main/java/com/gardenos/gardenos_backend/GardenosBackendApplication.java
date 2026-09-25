12152b5292482ef186022d22b135cb7b9a7abe2f
alvaromendoooo <alvaromendo2004@gmail.com>
2026-09-25 10:40:26 +0200

    (chore): project scaffold completed

---
+++ gardenos-backend/gardenos-backend/src/main/java/com/gardenos/gardenos_backend/GardenosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gardenos.gardenos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenosBackendApplication.class, args);
+	}
+
+}
---
+++ gardenos-backend/gardenos-backend/src/test/java/com/gardenos/gardenos_backend/GardenosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gardenos.gardenos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
